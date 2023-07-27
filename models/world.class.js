9af3837cd3b9110011038c9156ac3c3e24fdad8e
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2023-07-27 19:52:05 +0200

    display character in game

--- models/world.class.js
+++ models/world.class.js
@@ -5,9 +5,21 @@ class World {
         new Chicken(),
         new Chicken()
     ];
+    ctx;
+
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
 
 
     draw() {
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
 
+        for (let i = 0; i < this.enemies.length; i++) {
+            const chicken = this.enemies[i];
+            this.ctx.drawImage(chicken.img, chicken.x, chicken.y, chicken.width, chicken.height);
+        }
     }
 }
