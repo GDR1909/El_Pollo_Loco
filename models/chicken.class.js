57aa8d9088f6e88056fb2845d960ede63bb159fc
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2023-07-31 11:04:27 +0200

    added more background object and scaled the character and enemies

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,4 +1,8 @@
 class Chicken extends MoveableObject {
+    height = 60;
+    width = 60;
+    y = 360;
+
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
