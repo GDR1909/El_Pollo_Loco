48b7400fd1a158cf322fa0579daa1863d02f259c
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2023-08-16 15:31:11 +0200

    collision detection implemented

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -34,6 +34,31 @@ class MoveableObject {
     }
 
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+
+    drawFrame(ctx) {
+        if (this instanceof Character || this instanceof Chicken || this instanceof SmallChicken || this instanceof Endboss) {
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+    }
+
+
+    // character.isColliding(chicken);
+    isColliding(mo) {
+        return  this.x + this.width > mo.x &&
+                this.y + this.height > mo.y &&
+                this.x < mo.x &&
+                this.y < mo.y + mo.height;
+    }
+
+
     /**
      * 
      * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
