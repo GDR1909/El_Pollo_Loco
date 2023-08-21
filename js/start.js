038c3be9b159a7aed1761816829888bf8dab9259
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2023-08-21 20:08:30 +0200

    begin with the start screen

---
+++ js/start.js
@@ -0,0 +1,6 @@
+function start() {
+    document.getElementById('startScreen').classList.remove('startScreen');
+    document.getElementById('startScreen').classList.add('d-none');
+    document.getElementById('h1').classList.remove('v-hidden');
+    document.getElementById('canvas').classList.remove('d-none');
+}
