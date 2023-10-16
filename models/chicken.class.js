f5761daac2e824a9e346f927f89e3b84cf5754a5
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2023-10-16 19:52:00 +0200

    game plays animation and sound when the character jumps on a chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,4 @@
 class Chicken extends MoveableObject {
-    dead = false;
     height = 60;
     width = 60;
     y = 360;
@@ -9,12 +8,15 @@ class Chicken extends MoveableObject {
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
-    IMAGE_DEAD = 'img/3_enemies_chicken/chicken_normal/2_dead/dead.png';
+    IMAGE_DEAD = ['img/3_enemies_chicken/chicken_normal/2_dead/dead.png'];
+    dead_sound = new Audio('audio/chickenDead.mp3');
 
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGE_DEAD);
+
 
         this.x = 200 + Math.random() * 2000; // zufällige Zahl zwischen 200 und 700
         this.speed = 0.15 + Math.random() * 0.5;
@@ -34,8 +36,7 @@ class Chicken extends MoveableObject {
     }
 
     deadChicken() {
-        if (dead = true) {
-            this.playAnimation(this.IMAGE_DEAD);
-        }
+        this.dead_sound.play();
+        this.playAnimation(this.IMAGE_DEAD);
     }
 }
