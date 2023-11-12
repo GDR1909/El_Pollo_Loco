851a2f4f424177a313d669eb6d10db2d10b9312f
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2023-11-12 01:17:52 +0100

    commit

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -6,3 +6,65 @@ class Keyboard {
     SPACE = false;
     D = false;
 }
+
+
+window.addEventListener("keydown", (e) => {
+    if (world.character.characterIsDead) {
+        keyboard.LEFT = false;
+    } else if (e.keyCode == 37) {
+        keyboard.LEFT = true;
+    }
+
+    if (world.character.characterIsDead) {
+        keyboard.RIGHT = false;
+    } else if (e.keyCode == 39) {
+        keyboard.RIGHT = true;
+    }
+
+    if (e.keyCode == 38) {
+        keyboard.UP = true;
+    }
+
+    if (e.keyCode == 40) {
+        keyboard.DOWN = true;
+    }
+
+    if (world.character.characterIsDead) {
+        keyboard.SPACE = false;
+    } else if (e.keyCode == 32) {
+        keyboard.SPACE = true;
+    }
+
+    if (world.character.characterIsDead) {
+        keyboard.D = false;
+    } else if (e.keyCode == 68) {
+        keyboard.D = true;
+    }
+})
+
+
+window.addEventListener("keyup", (e) => {
+    if (e.keyCode == 37) {
+        keyboard.LEFT = false;
+    }
+
+    if (e.keyCode == 39) {
+        keyboard.RIGHT = false;
+    }
+
+    if (e.keyCode == 38) {
+        keyboard.UP = false;
+    }
+
+    if (e.keyCode == 40) {
+        keyboard.DOWN = false;
+    }
+
+    if (e.keyCode == 32) {
+        keyboard.SPACE = false;
+    }
+
+    if (e.keyCode == 68) {
+        keyboard.D = false;
+    }
+})
